6c025dbd3690602ea28bc36dd207bf20b33c0c6a
Yahyamohamed101 <yahyamohamed0001@gmail.com>
2021-06-22 21:33:01 +0300

    D

--- routes/web.php
+++ routes/web.php
@@ -53,4 +53,7 @@ Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function(){
     Route::get('/admin/dashboard',AdminDashboardComponent::class)->name('admin.dashboard');
 });
 
-
+// Dashboard For Admin
+Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function(){
+    Route::get('/admin/dashboard',AdminDashboardComponent::class)->name('admin.dashboard');
+});
