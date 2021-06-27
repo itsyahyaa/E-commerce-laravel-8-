b75765d84f4aad911995ea624a919943b8f1d8e5
Yahyamohamed101 <yahyamohamed0001@gmail.com>
2021-06-27 21:14:49 +0300

    D

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -13,7 +13,7 @@ class ShopComponent extends Component
         Cart::add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
         session()->flash('success_message','Item added in Cart');
         return redirect()->route('product.cart');
-        return redirect()->route('product.cart'); return redirect()->route;
+       
     }
 
     use WithPagination;
